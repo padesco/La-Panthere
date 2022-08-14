fe09fea6cd0a1e40939c745dd278a4a409e0bb12
padesco <pa.desco@live.com>
2022-08-14 15:56:39 +0200

    updated bootstrap

--- js/gmaps.js
+++ js/gmaps.js
@@ -17,3 +17,22 @@ function initMap() {
   }
   
   window.initMap = initMap;
+
+  //Get the button
+  let mybutton = document.getElementById("back-to-top");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+  scrollFunction();
+  };
+  
+  function scrollFunction() {
+  if (
+  document.body.scrollTop > 20 ||
+  document.documentElement.scrollTop > 20
+  ) {
+  mybutton.style.display = "block";
+  } else {
+  mybutton.style.display = "none";
+  }
+  }
