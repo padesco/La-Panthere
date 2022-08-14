fcea15df1c0b4cfc0036130be24b386b282aebd6
padesco <pa.desco@live.com>
2022-08-14 19:53:23 +0200

    titles and form updated

--- js/gmaps.js
+++ js/gmaps.js
@@ -16,7 +16,7 @@ function initMap() {
       map: map,
     });
   }
-  
+
   window.initMap = initMap;
 
   //Scroll to top button
@@ -37,4 +37,11 @@ function initMap() {
   } else {
   mybutton.style.display = "none";
   }
+  }
+  // When the user clicks on the button, scroll to the top of the document
+  mybutton.addEventListener("click", backToTop);
+
+  function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
   }
